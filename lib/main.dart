39f17811937a2ecd89c86c0bca9cb01f8300b9e3
titit <archiecakra1@gmail.com>
2021-01-08 06:02:47 +0700

    fluttter back button

--- lib/main.dart
+++ lib/main.dart
@@ -2,26 +2,79 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-void main() => runApp(MyApp());
+// void main() => runApp(MyApp());
+void main() => runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'SIM Mentari',
+    home: WebViewExample()));
 
-class MyApp extends StatelessWidget {
+class WebViewExample extends StatefulWidget {
+  @override
+  _WebViewExampleState createState() => _WebViewExampleState();
+}
+
+WebViewController controllerGlobal;
+
+Future<bool> _exitApp(BuildContext context) async {
+  var bool = await controllerGlobal.canGoBack();
+  if (bool) {
+    print("onwill goback");
+    controllerGlobal.goBack();
+    return Future.value(true);
+  } else {
+    Scaffold.of(context).showSnackBar(
+      const SnackBar(content: Text("No back history item")),
+    );
+    return Future.value(false);
+  }
+}
+
+class _WebViewExampleState extends State<WebViewExample> {
+  final Completer<WebViewController> _controller =
+      Completer<WebViewController>();
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'SIM Mentari',
-      home: Scaffold(
+    return WillPopScope(
+      onWillPop: () => _exitApp(context),
+      child: Scaffold(
         appBar: AppBar(
           title: Text('Katalog Toko Mentari Grosir'),
         ),
-        body: WebView(
-          initialUrl: "https://teeteet.site",
-          javascriptMode: JavascriptMode.unrestricted,
-        ),
+        body: Builder(builder: (BuildContext context) {
+          return WebView(
+            initialUrl: 'https://teeteet.site',
+            javascriptMode: JavascriptMode.unrestricted,
+            onWebViewCreated: (WebViewController webViewController) {
+              _controller.complete(webViewController);
+            },
+            navigationDelegate: (NavigationRequest request) {
+              print('allowing navigation to $request');
+              return NavigationDecision.navigate;
+            },
+            javascriptChannels: <JavascriptChannel>[
+              _toasterJavascriptChannel(context),
+            ].toSet(),
+            onPageFinished: (String url) {
+              print('Page finished loading: $url');
+            },
+          );
+        }),
       ),
     );
+  }
+
+  JavascriptChannel _toasterJavascriptChannel(BuildContext context) {
+    return JavascriptChannel(
+        name: 'Toaster',
+        onMessageReceived: (JavascriptMessage message) {
+          Scaffold.of(context).showSnackBar(
+            SnackBar(content: Text(message.message)),
+          );
+        });
   }
 }
